6f0d6f388a6f832f11be09d4b1cbe39304f342fd
Crunchy02 <w.sailer271@gmail.com>
2025-01-29 13:35:48 -0600

    Updated Dealer's Chances.py
    Added in that if dealer hits blackjack, it just moves on to the next hand to prevent blackjacks from being included in the player's choices.

--- Dealer's Chances.py
+++ Dealer's Chances.py
@@ -18,6 +18,8 @@ def preparingDeck(deck):
 
     return deck
 
+    
+
 #The actions of the dealer drawing until reaching at least 17 or busting
 def dealerActions(deck, dealersCards, reshuffle):
     #Checking the score of the cards dealt to the dealer
@@ -36,7 +38,7 @@ def dealerActions(deck, dealersCards, reshuffle):
             score = 7
         if dealersCards[1] == 11:
             dealersCards[1] = 1
-            score = 7
+            score = 7   
 
     #Having the dealer draw until score is at least 17
     while score < 17:
@@ -67,7 +69,7 @@ if __name__ == '__main__':
     startTime = time.time()
 
     #Opening excel file to store stats in
-    wb = openpyxl.load_workbook('Black Jack Data.xlsx')
+    wb = openpyxl.load_workbook('Blackjack Data.xlsx')
     ws = wb['Dealer Stats']
 
     #Makes a deck consisting of 6 standard 52 card decks
@@ -102,19 +104,26 @@ if __name__ == '__main__':
         if deck[1] == 0:
             deck = [deck[0]] + deck[2:]
             reshuffle = True
-
+        
         dealersHand = deck[:2]
         deck = deck[2:] + deck[:2]
         column = dealersHand[0] - 2
-        deck, bust, score, reshuffle = dealerActions(deck, dealersHand, reshuffle)
 
-        #Recording the outcome of this particular hand
-        if bust:
-            row = 5
-            dealerCardStats[row][column] += 1
-        else:
-            row = score - 17
-            dealerCardStats[row][column] += 1
+        score = 0
+        for i in range(len(dealersHand)):
+            score += dealersHand[i]
+        
+        #Checking if dealer has blackjack
+        if score != 21:
+            deck, bust, score, reshuffle = dealerActions(deck, dealersHand, reshuffle)
+
+            #Recording the outcome of this particular hand
+            if bust:
+                row = 5
+                dealerCardStats[row][column] += 1
+            else:
+                row = score - 17
+                dealerCardStats[row][column] += 1
 
         if reshuffle:
             deck = preparingDeck(deck)
@@ -129,6 +138,6 @@ if __name__ == '__main__':
         for rows in range(6):
             ws.cell(row=rows + 3, column=cols + 2).value = dealerCardStats[rows][cols] / columnTotals[cols] * 100
 
-    wb.save('Black Jack Data.xlsx')
+    wb.save('Blackjack Data.xlsx')
 
     print(time.time() - startTime)
